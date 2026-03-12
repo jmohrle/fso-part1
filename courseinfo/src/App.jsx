855283da47039f374446a7d091a45e6e1d54c9ba
Jim Mohrle <jpmohrle@outlook.com>
2026-03-12 16:51:25 -0400

    feat(courseinfo): course information step 4 (ex-1.4)

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -6,6 +6,7 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <p>{props.part} {props.exercises}</p>
   )
@@ -23,33 +24,36 @@ const Content = (props) =>{
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
-  <p>Number of exercises {props.total}</p>
+  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />      
-    </div>    
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
   )
 }
 
